0ddc2b6b51015fae71cba5406060d79b1a8d3c58
gnivler <gnivil@gmail.com>
2016-07-06 15:03:37 -0600

    fancy!

---
+++ SloppyJoesApp/MenuItem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SloppyJoesApp
+{
+    class MenuItem
+    {
+        public string Meat { get; private set; } 
+        public string Condiment { get; private set; }
+        public string Bread { get; private set; } 
+
+        public override string ToString() => $"{Meat} with {Condiment} on {Bread}";
+
+        public MenuItem(string meat, string condiment, string bread)
+        {
+            Meat = meat;
+            Condiment = condiment;
+            Bread = bread;
+        }
+    }
+}
---
+++ SloppyJoesApp/MenuMaker.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+
+namespace SloppyJoesApp
+{
+    class MenuMaker
+    {
+        public int NumberOfItems { get; set; }
+        public ObservableCollection<MenuItem> Menu { get; private set; }
+        public DateTime GeneratedDate { get; private set; }
+        public Random random = new Random();
+        private string[] Meats = { "Roast beef", "Salami", "Turkey", "Ham", "Pastrami" };
+        private string[] Condiments = { "yellow mustard", "brown mustard", "honey mustard", "mayo", "relish", "French dressing" };
+        private string[] Breads = { "rye", "white", "wheat", "pumpernickel", "Italian bread", "a roll" };
+
+        public MenuMaker()
+        {
+            Menu = new ObservableCollection<MenuItem>();
+            NumberOfItems = 10;
+            UpdateMenu();
+        }
+
+        public void UpdateMenu()
+        {
+            Menu.Clear();
+            GeneratedDate = DateTime.Now;
+            for (int i = 0; i < NumberOfItems; i++)
+            {
+                Menu.Add(CreateMenuItem());
+            }
+        }
+
+        private MenuItem CreateMenuItem()
+        {
+            string meat = Meats[random.Next(Meats.Length)];
+            string condiment = Condiments[random.Next(Condiments.Length)];
+            string bread = Breads[random.Next(Breads.Length)];
+            return new MenuItem(meat, condiment, bread);
+        }
+
+    }
+}
